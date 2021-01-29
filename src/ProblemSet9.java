c4cb1af53f8a4523613031ad5776a16508b5e612
unknown <Monga@1WP1186-LAP-01.business.rutgers.edu>
2021-01-29 10:29:49 -0500

    initial commit

---
+++ src/ProblemSet9.java
@@ -0,0 +1,42 @@
+public class ProblemSet9 {
+
+    public int[] evenOdd(int[] arr) {
+
+    }
+
+    public int[] notAlone(int[] arr, int alone) {
+
+    }
+
+    public int[] shiftLeft(int[] arr) {
+
+    }
+
+    public int[] fillIn(int start, int end) {
+
+    }
+
+    public boolean triple(int[] arr) {
+
+    }
+
+    public int pairs(int[] a, int[] b) {
+
+    }
+
+    public boolean twentyFour(int[] arr) {
+
+    }
+
+    public boolean fourteen(int[] arr) {
+
+    }
+
+    public int centeredAverage(int[] arr) {
+
+    }
+
+    public int outliers(int[] arr) {
+
+    }
+}
